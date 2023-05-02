dde72075c303451339d5ce317e1f3de615916c3c
Bruno Wagner <bruno.wagner.b.r@gmail.com>
2023-05-02 02:45:21 -0300

    fix: liberando cors

--- api/src/main/java/com/repeti/api/config/security/CorsConfig.java
+++ api/src/main/java/com/repeti/api/config/security/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.repeti.api.config.security;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,16 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Value("${security.alloworigin.port1}")
-    private String porta1;
-
-    @Value("${security.alloworigin.port2}")
-    private String porta2;
-    
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:" + porta1, "http://localhost:" + porta2)
-            .allowedMethods("GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT");
+            .allowedOriginPatterns("*")
+            .allowedMethods("GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS",  "HEAD", "TRACE", "CONNECT")
+            .allowedHeaders("X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization")
+            .allowCredentials(true);
     }    
 }
