281a3d439a2aa38e6e72288d8b3eee55a9b03180
v1d3rm3 <victorschinaider@outlook.com>
2023-05-09 21:23:28 -0300

    fix: correção do filtro de questão estudada

--- api/src/main/java/com/repeti/api/repository/QuestaoEstudadaRepository.java
+++ api/src/main/java/com/repeti/api/repository/QuestaoEstudadaRepository.java
@@ -7,13 +7,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.repeti.api.model.Nivel;
-import com.repeti.api.model.Questao;
 import com.repeti.api.model.QuestaoEstudada;
 
 public interface QuestaoEstudadaRepository extends JpaRepository<QuestaoEstudada, Integer> {
     @Query(value = "SELECT q FROM QuestaoEstudada q WHERE q.estudo.id = :estudoId")
     List<QuestaoEstudada> recuperarQuestaoEstudadasPorEstudo(@Param("estudoId") int estudoId);
 
-    @Query(value = "SELECT q FROM QuestaoEstudada q WHERE q.estudo.id = :estudoId AND q.nivel = :nivel")
-    List<QuestaoEstudada> recuperarQuestaoEstudadasPorEstudo(@Param("estudoId") int estudoId, @Param("nivel") Nivel nivel);
+    @Query(value = "SELECT q FROM QuestaoEstudada q JOIN q.questao qq WHERE q.estudo.id = :estudoId AND qq.nivel = :nivel")
+    List<QuestaoEstudada> recuperarQuestaoEstudadasPorEstudo(@Param("estudoId") int estudoId,
+            @Param("nivel") Nivel nivel);
 }
