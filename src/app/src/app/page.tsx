b9f34f132ecf3055c1d772870aefb5b336d837fc
v1d3rm3 <victorschinaider@outlook.com>
2023-07-09 20:51:23 -0300

    feat: adicionando redirecionamento para o dashboard em caso de sessao ativa

--- src/app/src/app/page.tsx
+++ src/app/src/app/page.tsx
@@ -1,9 +1,12 @@
 'use client'
 import Footer from '@/components/Footer'
+import { useSession } from 'next-auth/react'
 import { motion } from 'framer-motion'
 import { useEffect, useState } from 'react'
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+  const { data: session, status } = useSession()
   const [user, setUser] = useState('estudante')
   const users = [
     'professor',
@@ -15,6 +18,12 @@ export default function Home() {
     'acadêmico',
     'bibliotecário',
   ]
+
+  // redirecionar para o dashboard
+  if (session) {
+    redirect('/dashboard')
+  }
+  
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -33,12 +42,13 @@ export default function Home() {
           <div className="flex flex-row justify-center">
             <div className="mr-3">Todo</div>
             <motion.div animate={{ y: [-5, 5] }} transition={{ repeat: Infinity, duration: 2 }}>
-              <div className="text-sky-700">{user}</div>
+              <div className="text-sky-700 text-4xl font-bold">{user}</div>
             </motion.div>
           </div>
-          <div className="text-center">tem uma aba do repetir aberta</div>
+          <div className="text-center">tem uma aba do <b>repeti</b> aberta</div>
         </div>
-        <div className="flex items-start justify-center mt-16 w-full">
+        {/* colocar o botao para entrar..., caso esteja logado, redirecionar para o dashboard */}
+        {/* <div className="flex items-start justify-center mt-16 w-full">
           <div className="flex w-full bg-white p-1 rounded shadow-lg md:p-4 lg:w-2/4">
             <input
               type="text"
@@ -47,7 +57,7 @@ export default function Home() {
             />
             <button className="px-1 py-2 bg-sky-700 text-white rounded-r sm:px-6">Buscar</button>
           </div>
-        </div>
+        </div> */}
       </section>
 
       <Footer />
